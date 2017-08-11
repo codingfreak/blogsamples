30f9bb3560c46482d6d13903a60f32616c2af03a
Alexander Schmidt <info@codingfreaks.de>
2017-08-11 15:41:17 +0200

    NuGet updates

---
+++ MvvmSample/Logic/Logic.Ui/Models/ContactModel.cs
@@ -0,0 +1,20 @@
+﻿namespace codingfreaks.blogsamples.MvvmSample.Logic.Ui.Models
+{
+    using System;
+    using System.Linq;
+
+    using BaseTypes;
+
+    public class ContactModel : BaseModel
+    {
+        #region properties
+
+        public DateTimeOffset ContactDate { get; set; }
+
+        public string Notes { get; set; }
+
+        public string Title { get; set; }
+
+        #endregion
+    }
+}
