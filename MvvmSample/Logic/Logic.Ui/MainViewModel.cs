71c930216c9b48fc2d0beb9b8421caae55381043
Alexander Schmidt <info@codingfreaks.de>
2017-08-14 19:43:21 +0200

    Added binding samples for enumerables.

--- MvvmSample/Logic/Logic.Ui/MainViewModel.cs
+++ MvvmSample/Logic/Logic.Ui/MainViewModel.cs
@@ -3,8 +3,10 @@ namespace codingfreaks.blogsamples.MvvmSample.Logic.Ui
     using System;
     using System.Collections.Generic;
     using System.Collections.ObjectModel;
+    using System.ComponentModel;
     using System.Linq;
     using System.Threading.Tasks;
+    using System.Windows.Data;
 
     using BaseTypes;
 
@@ -44,12 +46,12 @@ namespace codingfreaks.blogsamples.MvvmSample.Logic.Ui
                                 while (Progress < 100)
                                 {
                                     DispatcherHelper.RunAsync(() => Progress += 5);
-                                    Task.Delay(500).Wait();
+                                    Task.Delay(100).Wait();
                                 }
                             });
-                    });                
+                    });
                 var personList = new List<PersonModel>();
-                for (var i = 0; i < 100; i++)
+                for (var i = 0; i < 10; i++)
                 {
                     personList.Add(
                         new PersonModel
@@ -59,10 +61,69 @@ namespace codingfreaks.blogsamples.MvvmSample.Logic.Ui
                         });
                 }
                 Persons = new ObservableCollection<PersonModel>(personList);
-
+                PersonsView = CollectionViewSource.GetDefaultView(Persons) as ListCollectionView;
+                PersonsView.CurrentChanged += (s, e) =>
+                {
+                    RaisePropertyChanged(() => PersonModel);
+                };
+                PersonsView.SortDescriptions.Clear();
+                PersonsView.SortDescriptions.Add(new SortDescription(nameof(PersonModel.Firstname), ListSortDirection.Ascending));
+                foreach (var item in Persons)
+                {
+                    item.PropertyChanged += PersonsOnPropertyChanged;
+                }
+                Persons.CollectionChanged += (s, e) =>
+                {
+                    if (e.NewItems != null)
+                    {
+                        foreach (INotifyPropertyChanged added in e.NewItems)
+                        {
+                            added.PropertyChanged += PersonsOnPropertyChanged;
+                        }
+                    }
+                    if (e.OldItems != null)
+                    {
+                        foreach (INotifyPropertyChanged removed in e.OldItems)
+                        {
+                            removed.PropertyChanged -= PersonsOnPropertyChanged;
+                        }
+                    }
+                };
                 OpenChildCommand = new RelayCommand(() => MessengerInstance.Send(new OpenChildWindowMessage("Hello Child!")));
                 SetSomeDateCommand = new RelayCommand<PersonModel>(person => person.Birthday = DateTime.Now.AddYears(-20));
+                AddPersonCommand = new RelayCommand(
+                    () =>
+                    {
+                        var newPerson = new PersonModel
+                        {
+                            Firstname = "Z(Firstname)"
+                        };
+                        Persons.Add(newPerson);
+                        PersonModel = newPerson;
+                    });
             }
+        }
+
+        #endregion
+
+        #region methods
+
+        /// <summary>
+        /// Event handler for property changes on elements of <see cref="Persons"/>.
+        /// </summary>
+        /// <param name="sender">The person model.</param>
+        /// <param name="e">The event arguments.</param>
+        private void PersonsOnPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == nameof(PersonModel.HasErrors) || e.PropertyName == nameof(PersonModel.IsOk))
+            {
+                return;
+            }
+            if (PersonsView.IsEditingItem || PersonsView.IsAddingNew)
+            {
+                return;
+            }
+            PersonsView.Refresh();
         }
 
         #endregion
@@ -70,29 +131,47 @@ namespace codingfreaks.blogsamples.MvvmSample.Logic.Ui
         #region properties
 
         /// <summary>
+        /// Adds a new person to the <see cref="Persons"/>.
+        /// </summary>
+        public RelayCommand AddPersonCommand { get; }
+
+        /// <summary>
         /// Opens a new child window.
         /// </summary>
         public RelayCommand OpenChildCommand { get; }
 
         /// <summary>
-        /// Takes a single person model and sets it's date to a certain value.
-        /// </summary>
-        public RelayCommand<PersonModel> SetSomeDateCommand { get; }
-
-        /// <summary>
         /// A person to edit.
         /// </summary>
-        public PersonModel PersonModel { get; set; } = new PersonModel();
+        public PersonModel PersonModel
+        {
+            get => PersonsView.CurrentItem as PersonModel;
+            set
+            {
+                PersonsView.MoveCurrentTo(value);
+                RaisePropertyChanged();
+            }
+        }
 
         /// <summary>
-        /// The list of person.
+        /// The view for binding the UI against the <see cref="Persons"/>.
         /// </summary>
-        public ObservableCollection<PersonModel> Persons { get; }
+        public ListCollectionView PersonsView { get; }
 
         /// <summary>
         /// Indicates the progress.
         /// </summary>
         public int Progress { get; set; }
+
+        /// <summary>
+        /// Adds a birthday to a person.
+        /// </summary>
+        public RelayCommand<PersonModel> SetSomeDateCommand { get; }
+
+        /// <summary>
+        /// The list of persons.
+        /// </summary>
+        private ObservableCollection<PersonModel> Persons { get; }
 
         #endregion
     }
