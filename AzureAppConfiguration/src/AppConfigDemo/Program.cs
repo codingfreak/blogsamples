e21b1aa8a2b26e37fff11400a5f24e7c02129917
devdeer-alex <alexander.schmidt@devdeer.com>
2023-02-22 09:30:27 +0100

    Added yaml samples

--- AzureAppConfiguration/src/AppConfigDemo/Program.cs
+++ AzureAppConfiguration/src/AppConfigDemo/Program.cs
@@ -6,8 +6,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Configuration.AzureAppConfiguration;
 
 var builder = new ConfigurationBuilder();
-var appName = Assembly.GetEntryAssembly()?
-    .GetName()
+var appName = Assembly.GetEntryAssembly()
+    ?.GetName()
     .Name ?? throw new ApplicationException("Could not read assembly name.");
 var connectionString = Environment.GetEnvironmentVariable("APPCONFIG_CONNECTIONSTRING") ?? throw new ApplicationException("No connection string defined.");
 var environment = Environment.GetEnvironmentVariable("DOTNET_ENVIRONMENT") ?? throw new ApplicationException("No environment defined.");
@@ -15,22 +15,24 @@ builder.AddAzureAppConfiguration(
     options =>
     {
         options.Connect(connectionString)
-            .ConfigureKeyVault(kvc => kvc.SetCredential(new DefaultAzureCredential()))
+            //.ConfigureKeyVault(kvc => kvc.SetCredential(new DefaultAzureCredential()))
+            //.Select($"{appName}:*", environment);
             .Select(KeyFilter.Any, environment);
     });
 var config = builder.Build();
-var configModel = new ConfigModel();
-config.Bind($"{appName}:Complex", configModel);
-Console.WriteLine(config[$"{appName}:DemoValue"]);
-Console.WriteLine(config[$"{appName}:OtherSample"]);
-Console.WriteLine(config[$"{appName}:Secret"]);
+//var configModel = new ConfigModel();
+//config.Bind("Complex", configModel);
+Console.WriteLine(config["DemoValue"]);
+//Console.WriteLine(config["OtherSample"]);
+//Console.WriteLine(config["Secret"]);
 
-
-class ConfigModel
+internal class ConfigModel
 {
+    #region properties
 
     public string? Foo { get; set; }
 
     public int Bar { get; set; }
-    
+
+    #endregion
 }
