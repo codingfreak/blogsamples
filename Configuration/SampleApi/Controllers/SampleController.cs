1b69649d4b5c425b5cdaa9be8e05a052649c603b
real-codingfreak <sprinter@codingfreaks.de>
2025-08-01 14:49:06 +0200

    Added sample API

---
+++ Configuration/SampleApi/Controllers/SampleController.cs
@@ -0,0 +1,28 @@
+﻿namespace SampleApi.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class SampleController : ControllerBase
+    {
+
+        private IConfiguration _configuration;
+
+        public SampleController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var result = new
+            {
+                DbConnection = _configuration["ConnectionStrings:MyDatabase"],
+                StorageConnection = _configuration.GetConnectionString("MyStorage")
+            };
+            return Ok(result);
+        }
+    }
+}
---
+++ Configuration/SampleApi/Program.cs
@@ -0,0 +1,25 @@
+﻿
+namespace SampleApi
+{
+    using Scalar.AspNetCore;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddControllers();
+            builder.Services.AddOpenApi();
+            var app = builder.Build();
+            if (app.Environment.IsDevelopment())
+            {
+                app.MapOpenApi();
+                app.MapScalarApiReference();
+            }
+            app.UseHttpsRedirection();
+            app.UseAuthorization();
+            app.MapControllers();
+            app.Run();
+        }
+    }
+}
