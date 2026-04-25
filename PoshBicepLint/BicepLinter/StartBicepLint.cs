f4f16ef5fd79954131732795b68373acab59f0f7
Alexander Schmidt <alexander.schmidt@devdeer.com>
2026-04-25 20:17:41 +0200

    CmdLet is working in principle

---
+++ PoshBicepLint/BicepLinter/StartBicepLint.cs
@@ -0,0 +1,23 @@
+﻿namespace codingfreaks.PoshBicepLint
+{
+    using System.Management.Automation;
+
+    [Cmdlet(VerbsLifecycle.Start, "BicepLint")]
+    [OutputType(typeof(string))]
+    public class StartBicepLint : PSCmdlet
+    {
+        #region methods
+
+        [Parameter(Mandatory = true, ValueFromPipeline = true)]
+        public string Message { get; set; } = null!;
+
+        /// <inheritdoc />
+        protected override void ProcessRecord()
+        {
+            WriteObject($"Hello: {Message}");
+            base.ProcessRecord();
+        }
+
+        #endregion
+    }
+}
